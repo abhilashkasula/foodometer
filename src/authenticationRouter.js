c37ddce00dfd12b17a413e5523435bb6c8576bed
Abhilash Kasula <58025056+abhilashkasula@users.noreply.github.com>
2022-06-08 23:02:39 +0530

    Add index

--- src/authenticationRouter.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const {signup, login, allowUnauthorized} = require('./authHandlers');
-
-const authentication = express.Router();
-
-authentication.post('/signup', [allowUnauthorized, signup]);
-authentication.post('/login', [allowUnauthorized, login]);
-
-module.exports = {authentication};
